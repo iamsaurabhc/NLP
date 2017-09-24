a2a228ce06fc1aa714089a111459d4c796d7e103
Saurabh Chakrabarty <saurabhc183@live.com>
2017-09-24 12:40:52 +0530

    Almost complete with all the updated files

--- Test.py
+++ Test.py
@@ -1,5 +1,8 @@
 import customNLTKDefn as custLib
 import nltk
+from xml.etree import ElementTree
+from xml.dom import minidom
+from lxml import etree
 
 '''
 from tkinter.filedialog import askopenfilename
@@ -20,26 +23,76 @@ fp.close()
 # Preprocess the Text extracted: Word processing, Tokenizing(Sentence & Word)
 text_preprocessed = custLib.preProcess(raw_text) #Regular Expression
 words = custLib.iePreprocess(text_preprocessed) #Information Extraction
-'''
+#print(words)
+
 #Get court Name
 court_name = custLib.extractCourtName(words)
-print("\nCOURT:\nCourt Name:  "+court_name)
+#print("\nCOURT:\nCourt Name:  "+court_name)
 
 #Get parties involved
 parties, pt, rt = custLib.extractParties(words)
-print("\nPARTIES:\nPetitioner:  "+parties[0]+"\tPetitioner Type:  "+pt)
-print("\nRespondent:  "+parties[1]+"\tRespondent Tye:  "+rt)
+#print("\nPARTIES:\nPetitioner:  "+parties[0]+"\tPetitioner Type:  "+pt)
+#print("\nRespondent:  "+parties[1]+"\tRespondent Tye:  "+rt)
 
-#Get Counsel groups for both Applicant and Respondent
-ac = custLib.extractApplicantCounselGroup(words)
-print("\nCOUNSEL GROUP:\nforApplicant:\n")
-for counsel in ac:
-    print(counsel)
-print("\nforRespondent:\n")
+#Get Counsel groups for both Petitioner and Respondent
+pc = custLib.extractPetitionerCounselGroup(words)
+#print("\nCOUNSEL GROUP:\nforApplicant:\n")
+#for counsel in pc:
+#    print(counsel)
+#print("\nforRespondent:\n")
 rc = custLib.extractRespondentCounselGroup(words)
-for counsel in rc:
-    print(counsel)
-'''
+#for counsel in rc:
+#    print(counsel)
+
 #Get Coram group
-judge,judgeType,day,date,month,year = custLib.extractCoramGroup(words)
-print("\nJudge: "+judge[0])
+judge,judgeType,day,dayType,date,month,year = custLib.extractCoramGroup(words)
+#print(dayType)
+
+# Extract Judgement Details
+judgementType,citeTitle,citation,actId,actTitle,sectionId,sectionTitle = custLib.extractJudgementSection(words)
+#print(sectionTitle)
+
+#Generate XML File
+xmlCase = etree.Element('Case')
+xmlDoc = etree.ElementTree(xmlCase)
+
+#Insert Court Details
+xmlCourt = etree.SubElement(xmlCase, 'Court')
+xmlCourtName = etree.SubElement(xmlCourt, 'CourtName')
+xmlCourtName.text = court_name
+
+#Insert Parties group
+xmlPartiesGroup = etree.SubElement(xmlCase, 'PartiesGroup')
+xmlParties = etree.SubElement(xmlPartiesGroup, 'Parties')
+xmlPetitionerGroup = etree.SubElement(xmlParties, 'PetitionerGroup')
+xmlPetitioner = etree.SubElement(xmlPetitionerGroup, 'Petitioner', Type=pt)
+xmlPetitioner.text = parties[0]
+
+xmlRespondentGroup = etree.SubElement(xmlParties, 'RespondentGroup')
+xmlRespondent = etree.SubElement(xmlRespondentGroup, 'Respondent', Type=rt)
+xmlPetitioner.text = parties[1]
+
+#Insert Counsel group
+xmlCounselGroup = etree.SubElement(xmlCase, 'CounselGroup')
+xmlforPetitioner = etree.SubElement(xmlCounselGroup, 'forPetitioner')
+for counsel in pc:
+    xmlCounselName = etree.SubElement(xmlforPetitioner, 'CounselName')
+    xmlCounselName.text = counsel
+
+xmlforRespondent = etree.SubElement(xmlCounselGroup, 'forRespondent')
+for counsel in rc:
+    xmlCounselName = etree.SubElement(xmlforPetitioner, 'CounselName')
+    xmlCounselName.text = counsel
+
+#Insert Coram group
+xmlCoramGroup = etree.SubElement(xmlCase, 'CoramGroup')
+xmlJudge = etree.SubElement(xmlCoramGroup, 'Judge', Position = judgeType[0])
+xmlJudge.text = judge[0]
+
+#Insert Date:
+for i,d in enumerate(day):
+    xmlDate = etree.SubElement(xmlCase, 'Date', Month = month[i], Date = date[i], Year = year[i], Type = dayType[i])
+    xmlDate.text = day[i]
+
+tree = etree.ElementTree(xmlCase)
+tree.write('ordjud.xml', pretty_print=True, xml_declaration=True,   encoding="utf-8")
--- customNLTKDefn.py
+++ customNLTKDefn.py
@@ -19,9 +19,11 @@ from nltk.corpus import stopwords
 from lxml import etree
 
 #Create XML
-case = etree.Element('Case')
-court = etree.Element('Court')
+xmlCase = etree.Element('Case')
+xmlCourt = etree.Element('Court')
 xmlCourtName = etree.Element('CourtName')
+xmlPartiesGroup = etree.Element('PartiesGroup')
+
 
 stop = stopwords.words('english')
 
@@ -69,7 +71,7 @@ def extractCourtName(string):
     court_name = string[string.index("judicature")+2]
     if(court_name == 'bombayordinary'):
         court_name= 'bombay'
-    global xmlCourtName = count_name
+    #global xmlCourtName = count_name
     return court_name
 
 def extractParties(words):
@@ -91,17 +93,17 @@ def extractParties(words):
     requiredTextTagged = nltk.pos_tag(requiredText)
     # Chunking with Regular Expressions
     chunkGram = r"""
-    Parties1: {<NN>?<JJ><NN.?>+<VB.?>}
+    Plaintiff: {<JJ><NNS><VBD>}
+    Defendant: {<NN><JJ><NN.?>+<VBD>}
     """
     chunkParser = nltk.RegexpParser(chunkGram)
     result = chunkParser.parse(requiredTextTagged)
-    i=-1
+    #print(result)
+    #result.draw()
     for res in result.subtrees():
-    	if res.label() == 'Parties1':
-    		i=i+1
-    		if(i==2):
-    			break
-    		parties.append(' '.join([l[0] for l in res.leaves()]))
+    	if(res.label() == 'Plaintiff' or res.label() == 'Defendant'):
+            parties.append(' '.join([l[0] for l in res.leaves()]))
+
     return parties, petitionerType, respondentType
 
 def getCounselDetailPara(words):
@@ -120,12 +122,12 @@ def getCounselDetailPara(words):
 
     return applicantCounselGroup,respondentCounselGroup
 
-def extractApplicantCounselGroup(words):
-    applicantCounselGroup = []
+def extractPetitionerCounselGroup(words):
+    petitionerCounselGroup = []
 
-    applicantCounsel,_ = getCounselDetailPara(words)
+    petitionerCounsel,_ = getCounselDetailPara(words)
 
-    advocatesTagged = nltk.pos_tag(applicantCounsel)
+    advocatesTagged = nltk.pos_tag(petitionerCounsel)
 
     # Chunking with Regular Expressions
     chunkGram = r"""
@@ -142,9 +144,9 @@ def extractApplicantCounselGroup(words):
         res.label() == 'applicantAdvocate1n3' or res.label() == 'applicantAdvocate2' or
         res.label() == 'applicantAdvocate4'   or res.label() == 'applicantAdvocate5'
         ):
-            applicantCounselGroup.append(' '.join([l[0] for l in res.leaves()]))
+            petitionerCounselGroup.append(' '.join([l[0] for l in res.leaves()]))
 
-    return applicantCounselGroup
+    return petitionerCounselGroup
 
 def extractRespondentCounselGroup(words):
     respondentCounselGroup = []
@@ -177,6 +179,7 @@ def extractCoramGroup(words):
     judge = []
     judgeType = []
     day = []
+    dayType = []
     date = []
     month = []
     year = []
@@ -191,19 +194,24 @@ def extractCoramGroup(words):
     chunkGram = r"""
     Judge: {<JJ><NN>{2,2}}
     Date: {<CD><N.*><JJ><CD>}
+    dayType: {<VBD>}
     JPosition: {<NN>}
     """
     chunkParser = nltk.RegexpParser(chunkGram)
     result = chunkParser.parse(coramAndDateTagged)
+    #result.draw()
 
     for res in result.subtrees():
         if(res.label() == 'Judge'):
             judge.append(' '.join([l[0] for l in res.leaves()]))
         if(res.label() == 'Date'):
             day.append(' '.join([l[0] for l in res.leaves()]))
+        if(res.label() == 'dayType'):
+            dayType.append(' '.join([l[0] for l in res.leaves()]))
     for res in result.subtrees():
         if(res.label() == 'JPosition'):
             judgeType.append(' '.join([l[0] for l in res.leaves()]))
+            break
     i=0
     for res in result.subtrees():
         if(res.label() == 'Date'):
@@ -216,6 +224,100 @@ def extractCoramGroup(words):
                     year.append(''.join(v[0]))
 
 
-    return judge,judgeType,day,date,month,year
+    return judge,judgeType,day,dayType,date,month,year
 
-def generateXMLFile()
+def extractJudgementSection(words):
+    judgementType = []
+    citePara = []
+    citeTitle = []
+    citation = []
+    actId = []
+    actTitle = []
+    actPara = []
+    sectionId = []
+    sectionTitle = []
+
+    #Find out judgement type
+    for w in words:
+        if(w == 'judgement' or w == 'order' or w == 'p c'):
+            judgementType = w
+
+    #Finding out Cite title and citation
+    for w in words:
+        if( w == 'cite' or w == 'cited'):
+            citePara = words[words.index(w)-25:words.index(w)]
+
+    citeParaTagged = nltk.pos_tag(citePara)
+
+    # Chunking with Regular Expressions
+    chunkGram = r"""
+    chunkTitle : {<NN><VBP><NN><VBN><NN.*>{6,6}<RBR>}
+    citation: {<JJ><NN><IN><NN>{2,2}}
+    """
+    chunkParser = nltk.RegexpParser(chunkGram)
+    result = chunkParser.parse(citeParaTagged)
+
+    for res in result.subtrees():
+        if(res.label() == 'chunkTitle'):
+            citeTitle.append(' '.join([l[0] for l in res.leaves()]))
+        if(res.label() == 'citation'):
+            citation.append(' '.join([l[0] for l in res.leaves()]))
+
+    #Finding out Act and Section Details
+    i=0
+    for w in words:
+        if(w == '1999'):
+            actPara = words[words.index(w)-10:words.index(w)+1]
+
+    actParaTagged = nltk.pos_tag(actPara)
+    # Chunking with Regular Expressions
+    chunkGram = r"""
+    actTitle : {<NN><NNS><VBP><CD>}
+    section: {<CD><NN>}
+    """
+    chunkParser = nltk.RegexpParser(chunkGram)
+    result = chunkParser.parse(actParaTagged)
+
+    for res in result.subtrees():
+        if(res.label() == 'actTitle'):
+            actTitle.append(' '.join([l[0] for l in res.leaves()]))
+        if(res.label() == 'section'):
+            sectionTitle.append(' '.join([l[0] for l in res.leaves()]))
+    for res in result.subtrees():
+        if(res.label() == 'actTitle'):
+            for i,l in enumerate(res.leaves()):
+                for j,a in enumerate(l[0]):
+                    #print(i,j,a)
+                    if(j==0 and i!=3):
+                        actId.append(a)
+                    if(i==3):
+                        actId.append(a)
+        if(res.label() == 'section'):
+            for l in res.leaves():
+                for a in l[0]:
+                    sectionId.append(a)
+
+    actId = ''.join(actId)
+    sectionId = ''.join(sectionId)
+    sectionId = actId+'-'+sectionId
+
+    return judgementType,citeTitle,citation,actId,actTitle,sectionId,sectionTitle
+
+    '''
+    i=0
+    for res in result.subtrees():
+        if(res.label() == 'Date'):
+            for l,v in enumerate(res.leaves()):
+                if(l == 0):
+                    date.append(''.join(v[0]))
+                if(l == 2):
+                    month.append(''.join(v[0]))
+                if(l == 3):
+                    year.append(''.join(v[0]))
+
+
+    return judge,judgeType,day,dayType,date,month,year
+    '''
+
+def generateXMLFile():
+    print()
